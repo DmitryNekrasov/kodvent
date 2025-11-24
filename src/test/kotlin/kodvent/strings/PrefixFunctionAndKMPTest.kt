f4ab2012e99daf8797cfe862c0bd33fda46ec2f8
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-11-24 16:13:47 +0400

    Add copyright and license header to `PrefixFunctionAndKMPTest.kt`

--- src/test/kotlin/kodvent/strings/PrefixFunctionAndKMPTest.kt
+++ src/test/kotlin/kodvent/strings/PrefixFunctionAndKMPTest.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025 Dmitry Nekrasov and kodvent library contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package kodvent.strings
 
 import kotlin.test.Test

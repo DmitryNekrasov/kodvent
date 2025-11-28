638b947203c211d285c22566626a5a0b30bd887e
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-11-28 12:52:38 +0400

    Implement segment tree construction logic in `SegmentTree` class

--- src/main/kotlin/kodvent/datastructures/SegmentTree.kt
+++ src/main/kotlin/kodvent/datastructures/SegmentTree.kt
@@ -8,4 +8,27 @@
 package kodvent.datastructures
 
 public class SegmentTree<T>(source: Collection<T>, private val operation: (T, T) -> T) {
+    private val size = source.size
+    private val tree = MutableList<T?>(size * 4) { null }
+
+    init {
+        if (source.isNotEmpty()) {
+            build(source)
+        }
+    }
+
+    private fun build(source: Collection<T>, nodeIndex: Int = 1, start: Int = 0, end: Int = size - 1) {
+        if (start == end) {
+            tree[nodeIndex] = source.elementAt(start)
+        } else {
+            val mid = start + (end - start) / 2
+            val leftNodeIndex = nodeIndex * 2
+            val rightNodeIndex = nodeIndex * 2 + 1
+
+            build(source, leftNodeIndex, start, mid)
+            build(source, rightNodeIndex, mid + 1, end)
+
+            tree[nodeIndex] = operation(tree[leftNodeIndex]!!, tree[rightNodeIndex]!!)
+        }
+    }
 }

add67041c64373af265ddc83d2ac559c0adc173d
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-11-30 18:21:48 +0400

    Add `Vertex` value class and suppress redundant visibility modifier warning

--- src/main/kotlin/kodvent/graphs/Graph.kt
+++ src/main/kotlin/kodvent/graphs/Graph.kt
@@ -3,12 +3,17 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
+@file:Suppress("RedundantVisibilityModifier")
+
 package kodvent.graphs
 
 class SequentialMapper<K, V>(val generator: (Int) -> V) {
     private val cache = mutableMapOf<K, V>()
     operator fun get(key: K) = cache.getOrPut(key) { generator(cache.size) }
 }
+
+@JvmInline
+public value class Vertex(val id: Int)
 
 class Graph {
 }

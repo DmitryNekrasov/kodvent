7e0ef424cf529d1fdd55eeb8dee7b1e4f35f55d7
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-11-30 18:12:40 +0400

    Add `Graph` class skeleton under `kodvent.graphs` package.

---
+++ src/main/kotlin/kodvent/graphs/Graph.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2025 Dmitry Nekrasov and kodvent library contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package kodvent.graphs
+
+class Graph {
+}

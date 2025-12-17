0e396e0e2af10f8d07cf8c9d9633aebd2c77634b
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-17 18:59:28 +0400

    Add `lowerBound` and `partitionPoint` utility functions for binary search operations

---
+++ src/main/kotlin/kodvent/boundsearch/boundSearch.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Dmitry Nekrasov and kodvent library contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+@file:Suppress("RedundantVisibilityModifier")
+
+package kodvent.boundsearch
+
+public fun <T : Comparable<T>> List<T>.lowerBound(element: T, fromIndex: Int = 0, toIndex: Int = size): Int {
+    return lowerBound(fromIndex, toIndex, element) { get(it) }
+}
+
+public fun partitionPoint(fromIndex: Int, toIndex: Int, predicate: (Int) -> Boolean): Int =
+    lowerBound(fromIndex, toIndex, true, predicate)
+
+public fun <T : Comparable<T>> lowerBound(
+    fromIndex: Int,
+    toIndex: Int,
+    element: T,
+    selector: (Int) -> T
+): Int {
+    var low = fromIndex
+    var high = toIndex - 1
+    while (low <= high) {
+        val mid = (low + high).ushr(1)
+        val cmp = compareValues(selector(mid), element)
+        if (cmp < 0) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return low
+}

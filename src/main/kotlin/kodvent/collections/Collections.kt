7157cdbf9a8d83a6e0302d2c7b77aa26024e23bb
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-18 13:57:06 +0400

    Add `lowerBound` extension function to `List` for binary search operations

--- src/main/kotlin/kodvent/collections/Collections.kt
+++ src/main/kotlin/kodvent/collections/Collections.kt
@@ -7,6 +7,13 @@
 
 package kodvent.collections
 
+import kodvent.boundsearch.partitionPoint
+
+public fun <T : Comparable<T>> List<T>.lowerBound(element: T, fromIndex: Int = 0, toIndex: Int = size): Int {
+    rangeCheck(size, fromIndex, toIndex)
+    return partitionPoint(fromIndex, toIndex) { get(it) < element }
+}
+
 private fun rangeCheck(size: Int, fromIndex: Int, toIndex: Int) {
     when {
         fromIndex < 0 -> throw IndexOutOfBoundsException("fromIndex ($fromIndex) is less than zero.")

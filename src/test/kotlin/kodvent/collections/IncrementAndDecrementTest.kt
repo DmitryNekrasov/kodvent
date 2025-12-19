77d364ef643bd853e175abaea0e8ff694379a3a6
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-19 11:00:04 +0400

    Move `increment` and `decrement` tests and extensions to `collections` package for better organization.

--- src/test/kotlin/kodvent/collections/IncrementAndDecrementTest.kt
+++ src/test/kotlin/kodvent/collections/IncrementAndDecrementTest.kt
@@ -1,9 +1,4 @@
-/*
- * Copyright 2025 Dmitry Nekrasov and kodvent library contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
-
-package kodvent
+package kodvent.collections
 
 import kotlin.test.Test
 import kotlin.test.assertEquals

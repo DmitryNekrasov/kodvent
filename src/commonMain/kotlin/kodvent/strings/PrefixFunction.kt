4c2b827ff489dac2996eca23a02174535fda2286
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2026-06-27 21:12:34 +0400

    Bump copyright year to 2025-2026 in remaining source files

--- src/commonMain/kotlin/kodvent/strings/PrefixFunction.kt
+++ src/commonMain/kotlin/kodvent/strings/PrefixFunction.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Dmitry Nekrasov and kodvent library contributors.
+ * Copyright 2025-2026 Dmitry Nekrasov and kodvent library contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
